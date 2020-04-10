6b2b77d94f0dc7231a462c36df12d7c39d826421
Tim Anderson-Bonsor <tim@andersonbonsor.com>
2020-04-10 12:31:22 +1000

    local updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,58 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["../../**/*.php","../../../../plugins/**/*.php"],
+  content: ["../../**/*.php", "../../../../plugins/cw-*/**/*.php"],
   // Whitelist selectors to stop Purgecss from removing them from your CSS.
-  
-  whitelist: ['text-8xl','lg:text-8xl','text-6xl','lg:text-6xl','text-5xl','lg:text-5xl','text-4xl','lg:text-4xl','text-2xl','lg:text-2xl','text-3xl','lg:text-3xl','text-xl','lg:text-xl','text-xs','lg:text-sm','text-lg','lg:text-lg','svg-inline--fa','lg:flex-1','text-black','text-slate','text-marine','text-navy','text-light-navy','text-cerulean-blue','text-very-light-pink','text-off-white','text-pale-brown','text-ultramarine','text-bluey-grey','text-light-blue-grey','text-ice-blue','text-dark-grey','text-white','text-red-brown','text-mahogany','text-toxic-green','text-almost-black','text-bright-yellow','text-bright-sky-blue','text-reddish-pink','text-brownish-grey','text-dark','text-pine'],
-  whitelistPatterns: [/^lg:max-w-/,/^max-w-/,/^fill-/,/^font-/,/^font-/,/^justify-/,/^items-/,/^flex-/,/^p-/,/^p.-/,/^m-/,/^m.-/,/^lg:p.-/,/^lg:m.-/,/^w-/,/^h-/],
+
+  whitelist: [
+    "clear-both",
+    "lg:-mb-32",
+    "lg:-mt-32",
+    "lg:flex-1",
+    "lg:pl-container",
+    "list-decimal",
+    "pl-container",
+    "svg-inline--fa",
+    "xl:pl-container",
+  ],
+  whitelistPatterns: [
+    /^fill-/,
+    /^flex-/,
+    /^lg:flex-/,
+    /^font-/,
+    /^h-/,
+    /^lg:h-/,
+    /^has-./,
+    /^items-/,
+    /^justify-/,
+    /^lg:max-w-/,
+    /^m-/,
+    /^lg:m-/,
+    /^m.-/,
+    /^lg:m.-/,
+    /^-m.-/,
+    /^max-w-/,
+    /^p-/,
+    /^lg:p-/,
+    /^p.-/,
+    /^lg:p.-/,
+    /^rounded-sm/,
+    /^text-./,
+    /^text-./,
+    /^hover:text-./,
+    /^w-/,
+    /^lg:w-/,
+  ],
   whitelistPatternsChildren: [/^fa-/],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
 
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    require('autoprefixer')({
-      'overrideBrowserslist': ['> 1%', 'last 2 versions']
+    require("autoprefixer")({
+      overrideBrowserslist: ["> 1%", "last 2 versions"],
     }),
-    ...process.env.NODE_ENV === "production"
+    ...(process.env.NODE_ENV === "production"
       ? [purgecss, require("cssnano")]
-      : []
-  ]
-}
-
+      : []),
+  ],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,73 +1,87 @@
 module.exports = {
   theme: {
+    colors: {
+      "transparent": "transparent",
+      "white": "var(--white)", // white: "#ffffff"
+      "black": "var(--black)", // black: "#000000"
+      "gray-100": "var(--gray-100)", // "ice-blue": "#edf2f5"
+      "gray-200": "var(--gray-200)", // "off-white": "#f3f2ee"
+      "gray-300": "var(--gray-300)", // "very-light-pink": "#d4d4d4"
+      "gray-400": "var(--gray-400)", // "light-blue-grey": "#bec8d2"
+      "gray-500": "var(--gray-500)", // "bluey-grey": "#98a2ae"
+      "gray-600": "var(--gray-600)", // "brownish-grey": "#6e6e6e"
+      "gray-700": "var(--gray-700)", // slate: "#4d5766"
+      "gray-800": "var(--gray-800)", // dark: "#273143"
+      "gray-900": "var(--gray-900)", // "dark-grey": "#1d1e20"
+      "blue-lightest": "var(--blue-lightest)", // "bright-sky-blue": "#00c9ff"
+      "blue-lighter": "var(--blue-lighter)", // "cerulean-blue": "#0065ed"
+      "blue-light": "var(--blue-light)", // "light-navy": "#124191"
+      "blue": "var(--blue)", // ultramarine: "#1000a9"
+      "blue-dark": "var(--blue-dark)", // marine: "#052253"
+      "blue-darker": "var(--blue-darker)", // navy: "#001235"
+      "green-light": "var(--green-light)", // "toxic-green": "#35b41f"
+      "green": "var(--green)", // pine: "#143010"
+      "green-dark": "var(--green-dark)", // "almost-black": "#11220e"
+      "gold": "var(--gold)", // "pale-brown": "#a19377"
+      "orange": "var(--orange)",
+      "red-light": "var(--red-light)", // "reddish-pink": "#ff3154"
+      "red": "var(--red)", // "red-brown": "#801515"
+      "red-dark": "var(--red-dark)", // mahogany: "#550000"
+      "yellow": "var(--yellow)", // "bright-yellow": "#fffa00"
+
+      // main colours
+      "primary": "var(--blue-lighter)",
+      "secondary": "var(--blue-light)",
+      "tertiary": "var(--blue-lightest)",
+      "lighter": "var(--gray-100)",
+      "light": "var(--gray-400)",
+      "mid": "var(--gray-600)",
+      "dark": "var(--gray-800)",
+      "darker": "var(--gray-900)",
+    },
     fontFamily: {
-      'light': ['NokiaPureHeadlineLight', 'serif'],
-      'regular': ['NokiaPureHeadlineRegular', 'serif'],
-      'ultra-light': ['NokiaPureHeadlineUltralight', 'serif'],
-      'bold': ['NokiaPureHeadlineBold', 'serif'],
+      "body": "var(--font-body)",
+      "heading": "var(--font-heading)",
     },
     fontWeight: {
-      'hairline': '100',
-      'thin': '200',
-      'lighter': '300',
-      'normal': '400',
-      'medium': '500',
-      'semibold': '600',
-      'bold': '700',
-      'extrabold': '800',
-      'black': '900',
+      thin: "var(--font-thin)",
+      light: "var(--font-light)",
+      normal: "var(--font-normal)",
+      bold: "var(--font-bold)",
     },
     extend: {
       fontSize: {
-        '5xl': '4rem',
-        '6xl': '6rem',
-        '8xl': '8rem',
+        "2xs": "0.75rem",
+        "4xl": "2.5rem",
+        "5xl": "4rem",
+        "6xl": "6rem",
+        "8xl": "8rem",
       },
       screens: {
+        xl: "1380px",
       },
       padding: {
-        'container-xs': ' 2rem',
-        'container-sm': '3rem',
-        'container-md': '4rem',
-        'container-lg': '5rem',
-        'container-xl': '8rem',
-        'container-2xl': '12rem',
+        container: "7vw",
+        "container-lg": "12vw",
+        "container-xl": "16vw",
+        "container-2xl": "20vw",
       },
-      colors: {
-        'black': '#000000',
-        'slate': '#4d5766',
-        'marine': '#052253',
-        'navy': '#001235',
-        'light-navy': '#124191',
-        'cerulean-blue': '#0065ed',
-        'very-light-pink': '#d4d4d4',
-        'off-white': '#f3f2ee',
-        'pale-brown': '#a19377',
-        'ultramarine': '#1000a9',
-        'bluey-grey': '#98a2ae',
-        'light-blue-grey': '#bec8d2',
-        'ice-blue': '#edf2f5',
-        'dark-grey': '#1d1e20',
-        'white': '#ffffff',
-        'red-brown': '#801515',
-        'mahogany': '#550000',
-        'toxic-green': '#35b41f',
-        'almost-black': '#11220e',
-        'bright-yellow': '#fffa00',
-        'bright-sky-blue': '#00c9ff',
-        'reddish-pink': '#ff3154',
-        'brownish-grey': '#6e6e6e',
-        'dark': '#273143',
-        'pine': '#143010'
-      }
+      width: {
+        "1/8": "12.5%",
+        "1/7": "14.2857143%",
+      },
     },
   },
   variants: {
-    margin: ['responsive', 'last'],
-    borderWidth: ['responsive', 'last'],
-    fontSize: ['responsive'],
+    margin: ["responsive", "last"],
+    borderWidth: ["responsive", "last"],
+    fontSize: ["responsive"],
+    wpBackgroundColor: ["hover"],
+    wpTextColor: ["hover"],
   },
   plugins: [
-    require('@tailwindcss/custom-forms'),
+    require("@tailwindcss/custom-forms"),
+    require("tailwindcss-wp-text")(),
+    require("tailwindcss-wp-background")(),
   ],
-}
+};
