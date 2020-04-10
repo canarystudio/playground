539f10935f6b8c25831974fe28bd8e697ac89d6b
Tim Anderson-Bonsor <canarystudio@users.noreply.github.com>
2020-04-10 12:23:24 +1000

    updates to css whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,58 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./public/**/*.html'],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["../../**/*.php", "../../../../plugins/cw-*/**/*.php"],
+  // Whitelist selectors to stop Purgecss from removing them from your CSS.
+
+  whitelist: [
+    "clear-both",
+    "lg:-mb-32",
+    "lg:-mt-32",
+    "lg:flex-1",
+    "lg:pl-container",
+    "list-decimal",
+    "pl-container",
+    "svg-inline--fa",
+    "xl:pl-container",
+  ],
+  whitelistPatterns: [
+    /^fill-/,
+    /^flex-/,
+    /^lg:flex-/,
+    /^font-/,
+    /^h-/,
+    /^lg:h-/,
+    /^has-./,
+    /^items-/,
+    /^justify-/,
+    /^lg:max-w-/,
+    /^m-/,
+    /^lg:m-/,
+    /^m.-/,
+    /^lg:m.-/,
+    /^-m.-/,
+    /^max-w-/,
+    /^p-/,
+    /^lg:p-/,
+    /^p.-/,
+    /^lg:p.-/,
+    /^rounded-sm/,
+    /^text-./,
+    /^text-./,
+    /^hover:text-./,
+    /^w-/,
+    /^lg:w-/,
+  ],
+  whitelistPatternsChildren: [/^fa-/],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
 
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-      ? [purgecss, require('cssnano')]
-      : []
-  ]
-}
+    require("tailwindcss"),
+    require("autoprefixer")({
+      overrideBrowserslist: ["> 1%", "last 2 versions"],
+    }),
+    ...(process.env.NODE_ENV === "production"
+      ? [purgecss, require("cssnano")]
+      : []),
+  ],
+};
